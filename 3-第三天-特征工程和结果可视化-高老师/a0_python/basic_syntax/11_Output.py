a2af58fbe66b069ce4deee25aa3fc4bea2bfdc12
wangp899 <68437761+wangp899@users.noreply.github.com>
2021-02-24 11:16:26 +0800

    Delete 3-第三天-特征工程和结果可视化-高老师 directory

--- 3-第三天-特征工程和结果可视化-高老师/a0_python/basic_syntax/11_Output.py
+++
@@ -1,6 +0,0 @@
-Dc2='Resources/InputTest2.txt'
-
-#Output plain text
-with open (Dc2,'w') as FileTP2:  #'w': the file will be overwritten
-	FileTP2.write("Test")
-
--- 3-第三天-特征工程和结果可视化-高老师/a0_python/basic_syntax/5_Tuple.py
+++
@@ -1,5 +0,0 @@
-
-tupleT1=(0,1,2,3,4,'Apple')
-print(tupleT1[5])
-# json = {key: value}
-# json 
